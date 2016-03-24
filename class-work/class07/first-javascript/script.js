476b142fdb2fdef6cb9a141f49ef7a82573201b0
Leah Bossio <leahbossio@leah-bossios-macbook-pro.local>
2016-03-23 20:52:11 -0400

    classwork & still working on HW

---
+++ class-work/class07/first-javascript/script.js
@@ -0,0 +1,19 @@
+alert('this is an alert. this is annoying');
+
+document.write('writing to the document, like a boss');
+
+var num1 = 10 * 10;
+var num2 = 100 / 10;
+var num3 = 50 - 17;
+var num4 = 50 + 17;
+var myName = 'Leah Bossio'
+
+document.write(num1);
+document.write(num2);
+document.write(num3);
+document.write(num4);
+document.write(myName);
+// document.write(num1, num2, num3, num4, myName)
+
+document.getElementById('title').style.color='red';
+document.getElementById('text-chunk').style.background = 'blue';
