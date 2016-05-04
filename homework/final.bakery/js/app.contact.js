480bcbdebd7cf59a85f7f3cae8a5330ea582c165
Leah Bossio <leahbossio@leah-bossios-macbook-pro.local>
2016-05-04 11:54:34 -0400

    In progress final

--- homework/final.bakery/js/app.contact.js
+++ homework/final.bakery/js/app.contact.js
@@ -2,23 +2,20 @@ $(document).ready(function() {
 
 	var map;
 
-    function initMap() {
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40.7127, lng: -74.0059},
+      zoom: 5
+    });
 
+    var marker = new google.maps.Marker({
+	    position: {lat: 40.7127, lng: -74.0059},
+      map: map,
+      title: 'New York, NY'
+     });
+  }
 
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 40.7127, lng: -74.0059},
-          zoom: 5
-        });
-
-        var marker = new google.maps.Marker({
-		position: {lat: 40.7127, lng: -74.0059},
-		map: map,
-		title: 'New York, NY'
-	});
-
-    }
-
-    initMap();
+  initMap();
 
 })
 
--- homework/final.bakery/js/app.shop.js
+++ homework/final.bakery/js/app.shop.js
@@ -1,31 +1,30 @@
  $(document).ready(function(){
-      $('.store').slick({
-        setting-centerMode: setting-value
-      });
-    });
+    // $('.store').slick();
 
-$('.center').slick({
-  centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
+  $('.store').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
       }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
+    ]
+  });
+
 });
